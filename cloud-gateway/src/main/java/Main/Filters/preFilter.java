8746955d46b81082f13dabbf4a8e2a6fc44aa67f
itqpleyva@gmail.com <itqpleyva@gmail.com>
2020-11-15 06:58:16 -0500

    adding ACL

--- cloud-gateway/src/main/java/Main/Filters/preFilter.java
+++ cloud-gateway/src/main/java/Main/Filters/preFilter.java
@@ -1,9 +1,10 @@
 package Main.Filters;
 
-
-
-
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.stereotype.Component;
@@ -28,11 +29,22 @@ public class preFilter implements GlobalFilter {
 
 AuthClient authClient;
 Boolean flag;
+Boolean flag_role;
 String role ;
 
   @Override
   public Mono<Void> filter(final ServerWebExchange exchange, final GatewayFilterChain chain) {
 
+
+      //building ACL
+      final Map<String, List<String>> routeByRoles = new HashMap<String, List<String>>();
+      routeByRoles.put("/auth/authenticate", Arrays.asList("ROLE_ADMIN","ROLE_USER","ROLE_MANAGER"));
+      routeByRoles.put("/auth/valid_token", Arrays.asList("ROLE_ADMIN","ROLE_USER","ROLE_MANAGER"));
+      routeByRoles.put("/micro2/message", Arrays.asList("ROLE_ADMIN","ROLE_USER","ROLE_MANAGER"));
+      routeByRoles.put("/micro1/message", Arrays.asList("ROLE_ADMIN","ROLE_USER","ROLE_MANAGER"));
+
+      //end Buld ACL
+      
       final ServerHttpRequest request = exchange.getRequest();
       final String path = request.getPath().toString();
       final String authenticate_path = "/auth/authenticate";
@@ -40,12 +52,17 @@ String role ;
       if (path.equals(authenticate_path)) {// si viene de authentication sigue de largo
 
           return chain.filter(exchange);
-      } else {
-          if (request.getHeaders().containsKey("Authorization")) {// preguntando por la cabecera y el token
+      } 
+
+      else 
+      
+      {
+          if (request.getHeaders().containsKey("Authorization") && request.getHeaders().containsKey("User")) {// preguntando por la cabecera y el token
 
               final String autho_headers = request.getHeaders().get("Authorization").get(0);
+              final String autho_user = request.getHeaders().get("User").get(0);
               final String token = autho_headers.replace("Bearer ", "");
-              final TokenValidrequest tokenRequest = new TokenValidrequest(token, "user2");
+              final TokenValidrequest tokenRequest = new TokenValidrequest(token, autho_user);
 
               final HttpHeaders headers = new HttpHeaders(); // preparando la peticion con resttemplate
               final RestTemplate restTemplate = new RestTemplate();
@@ -59,25 +76,32 @@ String role ;
                   final ResponseEntity<List> response = restTemplate.postForEntity(fooResourceUrl, request1,
                           List.class);
 
-                  flag = Boolean.parseBoolean((String) response.getBody().get(0));// guardando el boolean del si el
-                                                                                  // token es valido
+                  flag = Boolean.parseBoolean((String) response.getBody().get(0));// guardando el boolean del valid token                                                                                // token es valido
 
                   role = (String) response.getBody().get(1);// guardando el rol del usuario
 
+                  flag_role = routeByRoles.get(path).contains(role);//si ese rol tiene esa ruta permitida
+
+                  System.out.println("*****************************************El valor de is valid es : "+flag);
+                  System.out.println("*****************************************El user es : "+autho_user);
+                  System.out.println("*****************************************El role del user es : "+role);
+                  System.out.println("*****************************************El valor de is route authorized es : "+flag_role);
+
               } catch (final Exception e) {
 
-                  return this.onError(exchange, "Token invalido", HttpStatus.UNAUTHORIZED);
+                  return this.onError(exchange, "Failed Authorization", HttpStatus.UNAUTHORIZED);
               }
 
-              if (!flag) {// llamar metodo token validation de auth
+              if (!flag || !flag_role ){
 
-                  return this.onError(exchange, "Token invalido", HttpStatus.UNAUTHORIZED);
+                  return this.onError(exchange, "Failed Authorization", HttpStatus.UNAUTHORIZED);
+
               } else {
 
                   return chain.filter(exchange);
               }
           } else {
-              return this.onError(exchange, "Token invalido", HttpStatus.UNAUTHORIZED);
+              return this.onError(exchange, "Failed Authorization", HttpStatus.UNAUTHORIZED);
           }
       }
   }
--- spring-jwt-security/src/main/java/com/itqpleyva/springjwtsecurity/Controllers/MainController.java
+++ spring-jwt-security/src/main/java/com/itqpleyva/springjwtsecurity/Controllers/MainController.java
@@ -18,7 +18,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestBody;
